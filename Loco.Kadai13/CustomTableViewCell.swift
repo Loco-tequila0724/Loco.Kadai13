7d5080e482c43399a550a36b2d38b7b09451f8f3
Loco-tequila0724 <hayato09097699090@icloud.com>
2021-07-30 00:12:21 +0900

    課題13 　フィードバック済

--- Loco.Kadai13/CustomTableViewCell.swift
+++ Loco.Kadai13/CustomTableViewCell.swift
@@ -1,16 +1,16 @@
 import UIKit
 
 class CustomTableViewCell: UITableViewCell {
-    @IBOutlet weak private var checkMarkImage: UIImageView!
+    @IBOutlet weak private var checkMarkImageView: UIImageView!
     @IBOutlet weak private var fruitsLabel: UILabel!
-    func setCell(imageName: String, checkBool: Bool, textLabel: String) {
-        if checkBool == true {
-            checkMarkImage.image = UIImage(systemName: imageName)
-        } else {
-            checkMarkImage.isHidden = true
-        }
 
-        checkMarkImage.tintColor = .orange
-        fruitsLabel.text = textLabel
+    func configure(item: CheckItem) {
+        if item.isChecked {
+            checkMarkImageView.image = UIImage(systemName: "checkmark")
+        } else {
+            checkMarkImageView.image = nil
+        }
+        checkMarkImageView.tintColor = .orange
+        fruitsLabel.text = item.name
     }
 }
